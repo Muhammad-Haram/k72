543d8155791260743e6dc6b0b48675f683de738c
Muhammad-Haram <haramh643@gmail.com>
2025-09-20 17:29:27 -0700

    create folder structure and react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div className='text-6xl text-white'>App</div>
+    <div className='text-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/agence' element={<Agence />} />
+      </Routes>
+    </div>
   )
 }
 
