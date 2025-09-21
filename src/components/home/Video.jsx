0614e7e856bbdd5e0590b2ef5f0988111cc0f662
Muhammad-Haram <haramh643@gmail.com>
2025-09-20 18:51:28 -0700

    end 9/20/2025

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <div className='w-full h-full'>
+        <video className='w-full h-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+    </div>
   )
 }
 
