57f76695c1c43720a0fa59c24e5348d2158e0e02
Muhammad-Haram <haramh643@gmail.com>
2025-09-21 14:48:57 -0700

    commit

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomeBottomText = () => {
+
   return (
-    <div>HomeBottomText</div>
+    <div>
+
+      <div>
+        <p className='lg:w-[17vw] w-64 font-[font1] lg:text-lg text-xs lg:leading-relaxed leading-tight'>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
+      </div>
+
+      <div className='font-[font2] flex items-center justify-center gap-2 '>
+        <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
+          <Link className='text-[6vw]' to='/projects'>Projects</Link>
+        </div>
+        <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
+          <Link className='text-[6vw]' to='/agence'>agence</Link>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
