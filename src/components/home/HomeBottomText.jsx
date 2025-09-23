09625e97ba27b02a0543a374a0931a375523a423
Muhammad-Haram <haramh643@gmail.com>
2025-09-22 17:26:27 -0700

    commit

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -5,21 +5,14 @@ const HomeBottomText = () => {
 
   return (
     <div>
-
-      <div>
-        <p className='lg:w-[17vw] w-64 font-[font1] lg:text-lg text-xs lg:leading-relaxed leading-tight'>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
-      </div>
-
       <div className='font-[font2] flex items-center justify-center gap-2 '>
-        <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
+        <div className='border p-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
           <Link className='text-[6vw]' to='/projects'>Projects</Link>
         </div>
-        <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
+        <div className='border p-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase'>
           <Link className='text-[6vw]' to='/agence'>agence</Link>
         </div>
       </div>
-
     </div>
   )
 }
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import React, { useRef } from 'react'
 
 const Agence = () => {
+
+  gsap.registerPlugin(ScrollTrigger);
+  const imgDivRef = useRef(null)
+
+  useGSAP(function () {
+    gsap.to(imgDivRef.current, {
+      scrollTrigger: {
+        trigger: imgDivRef.current,
+        markers: true,
+        start: "top 18%",
+        end: "top -100%",
+        scrub:true,
+        pin: true,
+      }
+    })
+  })
+
   return (
-    <div>Agence</div>
+
+    <>
+
+      <div className='section1'>
+
+        <div ref={imgDivRef} className='absolute h-[20vw] w-[15vw] top-28 left-[30vw] rounded-3xl overflow-hidden'>
+          <img className='h-full w-full object-cover' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+        </div>
+
+        <div className='relative font-[font2] '>
+
+          <div className='mt-[55vh]'>
+            <h1 className='text-[20vw] text-center uppercase leading-[18vw]'>Soixan7e <br />
+              Douze</h1>
+          </div>
+
+          <div className='pl-[40%] mt-20'>
+            <p className='text-5xl leading-tight'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="section2 h-screen">
+
+      </div>
+
+    </>
   )
 }
 
